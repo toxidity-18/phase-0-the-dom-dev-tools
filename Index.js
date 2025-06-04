dfacc3150759ad0d9fa43727c7dc3febfbc5ce8a
toxidity-18 <desavigatunganga@gmail.com>
2025-06-04 21:02:18 +0300

    ...

--- Index.js
+++ Index.js
@@ -3,3 +3,12 @@
 
 // developer tools we use ctrl+shift+i
 // viewing the DOM we use element tab 
+// dom with js console 
+// speaking DOM with JS console
+// how chaining takes place within DOM nodes 
+document.querySelector('h1')// locates the first heading in that document 
+document.querySelector('h1').remove() // temporary remove the h1
+// storing node references inside variables 
+const heading = document.querySelector('h1'); // store the h1 node in a variable
+
+// find , select , remove , insert and adjust properties of element in the rendered page
